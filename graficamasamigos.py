8c20a85a2bd95251de49009655cc969c0d3b742a
PerezIngrid <35306786+PerezIngrid@users.noreply.github.com>
2018-01-17 12:18:34 -0600

    Clase 17 enero 2018.

---
+++ graficamasamigos.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([0,0,3,3,7,10,12,13,10,12,12,12,10,9,10,6,6,5,4,3]) 
+plt.ylabel('Numero de mascotas+amigos/as que he tenido durante mi vida') 
+plt.savefig('temp.png') 
+plt.show() 
+
